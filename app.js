cae04ca49320596b1ae8f8b015e2df7b81f0075d
ghigbie <gehigbie@gmail.com>
2017-02-12 23:44:30 +0000

    added switch statement

--- app.js
+++ app.js
@@ -6,7 +6,30 @@ app.get("/", function(req, res){
 });
 
 app.get("/speak/:animal/", function(req, res){
-   res.send("<h1>stuff</h1>"); 
+    var animal = animal.toLowercase();
+    var animalSay = "grrr.";
+    switch(animal){
+        case "pig":
+            animalSay = "\"Oink!\"";
+            break;
+        case "cow":
+            animalSay = "\"Moo!\"";
+            break;
+        case "dog":
+            animalSay = "\"Woof!\"";
+            break;
+        case "cat":
+            animalSay = "\"Meow!\"";
+            break;
+        default:
+            animalSay = "nothing at all...";
+    }
+    
+   res.send("<h1>The " + animal + " says" + animalSay + "</h1>"); 
+});
+
+app.get("*", function(req, res){
+    res.send("<h1>There are no animals to be found here!</h1>");
 });
 
 app.listen(process.env.PORT, process.env.IP, function(){
